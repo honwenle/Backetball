e9170b20da6de88604964a2a244aabd8e72bd682
noclip <noclip@foxmail.com>
2019-08-01 09:41:47 +0800

    横向

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var can = document.getElementById('cvs')
 var ctx = can.getContext('2d')
 can.width = WIDTH
 can.height = HEIGHT
-var oX = 50, oY = 0, dirY = 1, speedY = 0
+var oX = 0, dirX = 1, speedX = 20,
+    oY = 200, dirY = -1, speedY = 20,
+    g = 1, m = .02
 
 var imgBall = new Image()
 imgBall.onload = init
@@ -19,20 +21,31 @@ function calc() {
   speedY += dirY
   if (speedY < 0) {
     speedY = 0
-  }
-  if (speedY == 0) {
-    if (dirY == 1) {
-      dirY = 0
-    }
     dirY = 1
   }
-  oY += dirY * speedY
-  if (oY + 50 > HEIGHT) {
-    oY = HEIGHT - 50
+  if (speedX < 0) {
+    speedX = 0
+    dirX = 0
   }
   if (oY + 50 == HEIGHT) {
+    speedX -= m
+  }
+  oY += dirY * speedY
+  oX += dirX * speedX
+  if (oY + 50 > HEIGHT) {
+    oY = HEIGHT - 50
     dirY = -1
     speedY *= .9
+  }
+  if (oX + 50 > WIDTH) {
+    oX = WIDTH - 50
+    dirX = -1
+    speedX *= .8
+  }
+  if (oX < 0) {
+    oX = 0
+    dirX = 1
+    speedX *= .8
   }
 }
 function render() {
